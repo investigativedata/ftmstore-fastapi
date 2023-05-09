4606c9b30da34e328e4b80cd076eb9831829f976
Simon Wörpel <simon.woerpel@pm.me>
2023-05-09 18:36:12 +0200

    Dehydration: no properties at all

--- ftmstore_fastapi/util.py
+++ ftmstore_fastapi/util.py
@@ -3,7 +3,6 @@ from typing import Any
 
 import pycountry
 from followthemoney import model
-from followthemoney.model import registry
 from followthemoney.proxy import E
 from nomenklatura.entity import CE, CompositeEntity
 
@@ -21,20 +20,16 @@ def get_dehydrated_proxy(
 ) -> CE:
     """
     reduce proxy payload to only include 1 name (caption)
-    and featured properties
+    and optionally context
     """
     proxy = get_proxy(data)
-    dehydrated = get_proxy({"id": proxy.id, "schema": proxy.schema.name})
+    dehydrated = get_proxy(
+        {"id": proxy.id, "schema": proxy.schema.name, "caption": proxy.caption}
+    )
     if include_context:
         dehydrated.datasets = proxy.datasets
         dehydrated.referents = proxy.referents
         dehydrated.context = proxy.context
-    for prop in proxy.schema.featured:
-        prop = proxy.schema.properties[prop]
-        if prop.type == registry.name:
-            dehydrated.add("name", proxy.caption)
-        else:
-            dehydrated.add(prop, proxy.get(prop))
     return dehydrated
 
 
