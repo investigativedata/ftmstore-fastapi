a7a7ba2a119a751d09380382ed3da10863873a18
Simon Wörpel <simon.woerpel@pm.me>
2024-07-12 09:59:44 +0200

    🚧 (cache) Disable cache via None key

--- ftmstore_fastapi/cache.py
+++ ftmstore_fastapi/cache.py
@@ -6,5 +6,7 @@ from ftmstore_fastapi import settings
 PREFIX = f"ftmstore_fastapi:{settings.VERSION}:{slugify(settings.TITLE)}"
 
 
-def get_cache_key(request: Request, *args, **kwargs) -> str:
+def get_cache_key(request: Request, *args, **kwargs) -> str | None:
+    if not settings.CACHE:
+        return None
     return f"{PREFIX}:{slugify(str(request.url))}"
